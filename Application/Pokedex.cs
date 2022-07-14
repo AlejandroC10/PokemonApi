5a72b02c9942f258df33572c4399fe3eef168ddd
AlejandroC10 <106809387+AlejandroC10@users.noreply.github.com>
2022-07-14 13:20:37 +0200

    [feature] Implemented search pokemon with id 2

--- Application/Pokedex.cs
+++ Application/Pokedex.cs
@@ -6,20 +6,49 @@ public class Pokedex
 {
     public Pokemon FindPokemonById(int id)
     {
-        var fakeNames = new Dictionary<string, string>
+        Dictionary<string,string> fakeNames;
+        Dictionary<string,int> fakeStats;
+        List<string> fakeTypes;
+        if (id ==2)
+        {
+            fakeNames = new Dictionary<string, string>
+            {
+                {"english", "Ivysaur"},
+                {"japanese", "フシギソウ"},
+                {"chinese", "妙蛙草"},
+                {"french", "Herbizarre"}
+            };
+            fakeStats = new Dictionary<string, int>
+            {
+                {"HP", 60},
+                {"Attack", 62},
+                {"Defense", 63},
+                {"Sp. Attack", 80},
+                {"Sp. Defense", 80},
+                {"Speed", 60}
+            };
+            fakeTypes = new List<string>
+            {
+                "Grass",
+                "Poison"
+            };
+            return new Pokemon(2,fakeNames,fakeTypes,fakeStats);
+        }
+        
+        
+        fakeNames = new Dictionary<string, string>
         {
             {"english", "Bulbasaur"}
         };
-        var fakeStats = new Dictionary<string, int>
+        fakeStats = new Dictionary<string, int>
         {
             {"HP", 45}
         };
-        var fakeTypes = new List<string>
+        fakeTypes = new List<string>
         {
             "Grass",
         };
         
-         
         return new Pokemon(1,fakeNames,fakeTypes,fakeStats);
     }
 }
