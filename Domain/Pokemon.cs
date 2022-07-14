87dc4357a77f77b52ce0292f52541f31f4477fe0
AlejandroC10 <106809387+AlejandroC10@users.noreply.github.com>
2022-07-14 11:55:46 +0200

    [test] Added Acceptance FindPokemonById

---
+++ Domain/Pokemon.cs
@@ -0,0 +1,46 @@
+﻿namespace Domain;
+
+public class Pokemon
+{
+  public int Id { get; set; }
+
+  public Dictionary<string, string> Name { get; set; }
+
+  public List<string> Type { get; set; }
+
+  public Dictionary<string, int> Stats { get; set; }
+
+  public Pokemon(int id, Dictionary<string,string> name, List<string> type, Dictionary<string,int> stats)
+  {
+    Id = id;
+    Name = name;
+    Type = type;
+    Stats = stats;
+  }
+}
+
+/*
+  {
+    "id": 1,
+    
+    "name": {
+      "english": "Bulbasaur",
+      "japanese": "フシギダネ",
+      "chinese": "妙蛙种子",
+      "french": "Bulbizarre"
+    },
+    
+    "type": [
+      "Grass",
+      "Poison"
+    ],
+    
+    "base": {
+      "HP": 45,
+      "Attack": 49,
+      "Defense": 49,
+      "Sp. Attack": 65,
+      "Sp. Defense": 65,
+      "Speed": 45
+    }
+*/
--- PokemonApi.Tests/Controllers/PokemonControllerAcceptanceTest.cs
+++ PokemonApi.Tests/Controllers/PokemonControllerAcceptanceTest.cs
@@ -1,20 +1,53 @@
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Domain;
+using FluentAssertions;
+using Newtonsoft.Json;
 using Xunit;
 using PokemonApi;
 
 namespace PokemonApiTests.Controllers;
 
-public class PokemonControllerAcceptanceTest: IClassFixture<CustomWepApplicationFactory<Program>>
+public class PokemonControllerAcceptanceTestShould: IClassFixture<CustomWepApplicationFactory<Program>>
 {
     private readonly HttpClient client;
 
-    public PokemonControllerAcceptanceTest(CustomWepApplicationFactory<Program> factory)
+    public PokemonControllerAcceptanceTestShould(CustomWepApplicationFactory<Program> factory)
     {
         client = factory.CreateClient();
     }
+    
     [Fact]
-    public void FindPokemonByIdAcceptance()
+    public async Task FindPokemonById()
     {
+        var response = await client.GetAsync("/Pokemon/1");
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var foundedPokemon = JsonConvert.DeserializeObject<Pokemon>(responseContent);
 
+        var fakeNames = new Dictionary<string, string>
+        {
+            {"english", "Bulbasaur"},
+            {"japanese", "フシギダネ"},
+            {"chinese", "妙蛙种子"},
+            {"french", "Bulbizarre"}
+        };
+        var fakeStats = new Dictionary<string, int>
+        {
+            {"HP", 45},
+            {"Attack", 49},
+            {"Defense", 49},
+            {"Sp. Attack", 65},
+            {"Sp. Defense", 65},
+            {"Speed", 45}
+        };
+        var fakeTypes = new List<string>
+        {
+            "Grass",
+            "Poison"
+        };
+        var fakePokemon = new Pokemon(1,fakeNames,fakeTypes,fakeStats);
+
+        foundedPokemon.Should().Equals(fakePokemon);
     }
 }
