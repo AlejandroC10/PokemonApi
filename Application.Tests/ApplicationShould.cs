7463573b641222759ea3a413f2341f3b532e57f3
AlejandroC10 <106809387+AlejandroC10@users.noreply.github.com>
2022-07-14 13:01:00 +0200

    [test] Added in aplication ReturnPokemonWithId2

--- Application.Tests/ApplicationShould.cs
+++ Application.Tests/ApplicationShould.cs
@@ -15,4 +15,35 @@ public class ApplicationShould
 
         expectedPokemon.Should().BeOfType<Pokemon>();
     }
+    
+    [Fact]
+    public void ReturnPokemonWithId2()
+    {
+        var pokedex = new Pokedex();
+        var expectedPokemon = pokedex.FindPokemonById(2);
+        var fakeNames = new Dictionary<string, string>
+        {
+            {"english", "Ivysaur"},
+            {"japanese", "フシギソウ"},
+            {"chinese", "妙蛙草"},
+            {"french", "Herbizarre"}
+        };
+        var fakeStats = new Dictionary<string, int>
+        {
+            {"HP", 60},
+            {"Attack", 62},
+            {"Defense", 63},
+            {"Sp. Attack", 80},
+            {"Sp. Defense", 80},
+            {"Speed", 60}
+        };
+        var fakeTypes = new List<string>
+        {
+            "Grass",
+            "Poison"
+        };
+        var fakePokemon = new Pokemon(2,fakeNames,fakeTypes,fakeStats);
+
+        expectedPokemon.Should().BeEquivalentTo(fakePokemon);
+    }
 }
